59af2c31154d586f627a567154412abad6ff6fec
Eddison Pinedo Espinoza <91136098+EddisonPinedoEsp@users.noreply.github.com>
2022-11-24 19:50:59 -0500

    capital

--- src/views/CapitalsGameView.vue
+++ src/views/CapitalsGameView.vue
@@ -1,63 +1,58 @@
 <script>
-import countries from "/src/datasets/countries.json";
-import CountryComponent from "../components/CountryComponent.vue";
-
 export default {
-  name: "CapitalsGameView",
-  components: {
-    CountryComponent,
-  },
-  data() {
-    return {
-      country: {},
-      capital: "",
-      puntaje: 0,
+  name: "CountryComponent",
+  props: [
+    "name",
+    "capital",
+    "currency_name",
+    "currency",
+    "region",
+    "code",
+    //TODO: crear una propiedad para determinar si al componente se le puede hacer click
+    props:{
+      text: String
     };
-  },
-  created() {
-    this.country = countries[Math.floor(Math.random() * countries.length)];
-  },
-  methods: {
-    setCapital(e) {
-      //TODO: implementar. fija la capital del input.
+    methods:{
+      handleclick(){
+        this.$emit("click";)
+      }
+    
     },
-    adivinarCapital() {
-      //TODO: implementar. verifica se la adivinanza es correcta.
-    },
-  },
 };
 </script>
 
 <template>
-  <h1>Puntaje: {{ puntaje }}</h1>
-  <div class="countries">
-    <!-- TODO: usar los metodos definidos arriba dentro del input para llenar el estado de la capital a adivinar-->
-    <input
-      placeholder="Adivina la capital"
+  <!--TODO: Utilizar la propiedad creada para controlar el click. Cuando el componente es "clickeable", redirigir a la pagina del pais.
+  Mira las rutas para ver como redirigir al usuario.-->
+  <button @click ="clickeable">{{text}}</button>
+  <div
+  >
+  <!--TODO: Poblar el HTML con las propiedades. Usar https://countryflagsapi.com/png/${code}-->
+  <img
+      width="200"
+      height="200"
     />
-    <button @click="adivinarCapital">Adivina!</button>
-    <div class="countries-container">
-      <!--TODO: CREAR EL COMPONENTE PARA VISUALIZAR EL PAIS-->
-    </div>
   </div>
 </template>
 
 <style scoped>
-@media (min-width: 1024px) {
-  .countries {
-    align-items: center;
-  }
+.country {
+  padding: 2px;
+  white-space: nowrap;
+  width: 350px;
+  height: 350px;
+  border-style: solid;
+  border-radius: 5px;
+  border-color: grey;
+}
 
-  input {
-    line-height: 2em;
-  }
+.country-hover:hover {
+  background-color: rgb(49, 49, 49);
+  cursor: pointer;
+}
 
-  .countries-container {
-    margin-left: 35%;
-    align-items: center;
-    overflow-y: auto;
-    vertical-align: middle;
-    padding: 10px;
-  }
+h1 {
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
